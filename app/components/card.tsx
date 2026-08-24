72796e5cb76d55415e0b0dacca6c2bb215d8a6d6
Kevinistic <appealergt@gmail.com>
2026-08-24 12:27:00 +0700

    hotfix: removed removed vars

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -7,13 +7,8 @@ export type CardItem = {
   Limited: string;
   Gamepass: string;
   Engine: string;
-  RimsUrl: string;
-  rgb_0: string;
-  rgb_1: string;
-  rgb_2: string;
   Legacy: boolean;
   Inaccurate: boolean;
-  Rims: string;
   New: boolean;
   event: boolean;
   lostmedia: boolean;
